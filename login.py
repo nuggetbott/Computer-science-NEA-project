88a5635526bdab128c45d7fa295f858c2458e704
nuggetbott <kedrianng@gmail.com>
2026-08-09 23:21:35 +0800

    Login/Register from CLI base to GUI *

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
-import hashlib
-
+import hashlib 
+import tkinter as tk
+from tkinter import messagebox
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
@@ -22,35 +23,47 @@ def store_acc(username, password):
 
 
 def register():
-    username = input("Enter your username: ")
+
+    username = entry_username.get()
     if check_exist_user(username):
-        print("Account already exists.")    
+        messagebox.showerror("Error", "Account already exists.")
         return
 
-    password = input("Enter your password: ")
+    password = entry_password.get()
     store_acc(username, password)
-    print("Account created successfully.")
+    messagebox.showinfo("Success", "Account created successfully.")
 
 def login():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
+    username = entry_username.get()
+    password = entry_password.get()
     hashed_password = hash_password(password)
 
     try:
         with open("account.txt", "r") as f:
             for line in f:
                 stored_username, stored_password = line.strip().split(",")
-                if stored_username == username and stored_password == hash_password(password):
-                    print("Login successful.")
+                if stored_username == username and stored_password == hashed_password:
+                    messagebox.showinfo("Success", "Login successful.")
                     return True
     except FileNotFoundError:
         pass
 
-    print("Invalid username or password.")
+    messagebox.showerror("Error", "Invalid username or password.")
     return False
 
-option = input("Do you want to register or login? (register/login): ").strip().lower()
-if option == "register":
-    register()
-elif option == "login":
-    login()
+root = tk.Tk()
+root.title("Login/Register")
+tk.Label(root,text="username").grid(row=0, column=0, padx=10, pady=10)
+tk.Label(root,text="password").grid(row=1, column=0, padx=10, pady=10)
+
+entry_username = tk.Entry(root)
+entry_password = tk.Entry(root, show="*")
+entry_username.grid(row=0, column=1, padx=10, pady=10)
+
+entry_password.grid(row=1, column=1, padx=10, pady=10)
+
+button = tk.Button(root, text="register", width=25, command=register)
+button.grid(row=2, column=0, columnspan=2, pady=10)
+button = tk.Button(root, text="login", width=25, command=login)
+button.grid(row=3, column=0, columnspan=2, pady=10)
+root.mainloop()
