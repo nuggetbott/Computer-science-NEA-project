42c565627954b5ed6c88db566e5d024d00119dd8
nuggetbott <kedrianng@gmail.com>
2026-08-31 21:45:23 +0800

    made detect angle into a function

--- face_recongition.py
+++ face_recongition.py
@@ -25,6 +25,18 @@ options = vision.FaceLandmarkerOptions(
     output_face_blendshapes = True,
     output_facial_transformation_matrixes = True
 )
+
+def detect_angle(result):
+    if result.face_landmarks:
+        landmarks = result.face_landmarks[0]
+        right_eye = landmarks[33] # corner on right eye
+        left_eye = landmarks[263] # corner on left eye
+        diff_x = right_eye.x - left_eye.x
+        diff_y = right_eye.y - left_eye.y
+        angle = math.degrees(math.atan2(diff_y,diff_x))
+        return angle
+    return None
+
 with vision.FaceLandmarker.create_from_options(options) as landmarker:
     cap = cv2.VideoCapture(0) # Opens the webcam
     if not cap.isOpened():
@@ -43,16 +55,7 @@ with vision.FaceLandmarker.create_from_options(options) as landmarker:
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_color)
             timestamp = int(time.time()*1000) # creates a time reference for mediapipe and converts it into miliseconds.
             result = landmarker.detect_for_video(mp_image,timestamp)
-
-            if result.face_landmarks:
-                landmarks = result.face_landmarks[0] # list of all 476 facial landmarks
-                right_eye = landmarks[33] # corner on right eye
-                left_eye = landmarks[263] # corner on left eye
-                diff_x = right_eye.x - left_eye.x
-                diff_y = right_eye.y - left_eye.y
-
-                angle = math.degrees(math.atan2(diff_y,diff_x))
-                print(angle)
+            print(detect_angle(result))
     finally:
         cap.release()
         cv2.destroyAllWindows()
